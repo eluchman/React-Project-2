fe4c86743d8c4a8ca148a9cf3cd1d3fc6476c4d5
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-03-05 15:16:13 -0500

    Add files via upload

---
+++ counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 7,
+    imageUrl: 'https://picsum.photos/200'
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.state.imageUrl} alt=""/>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
