2dc944451f77a7a817ee627a91c0183569353ff3
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-03-11 12:39:43 -0400

    Add files via upload

--- counter.jsx
+++ counter.jsx
@@ -2,27 +2,56 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    value: this.props.value
   };
 
+  //constructor() {
+  //super();
+  //this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
+
+  // renders button onClick event, methods, counter method,
+
   render() {
+    console.log(this.props);
+
     return (
-      <React.Fragment>
+      <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <button
+          onClick={this.doHandleIncrement}
+          //onClick={() => this.handleIncrement(product) }
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
+  //helper method...giving color to badge/counter
+
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
+
+   //helper method...created counter
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
